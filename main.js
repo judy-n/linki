ff88de128e59e3c50570876ebd61718b25eb031b
judy-n <judynaamani@icloud.com>
2022-01-27 17:59:17 -0500

    some basic UI

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { menubar } = require('menubar');
 
 const mb = menubar();
-
+require('electron-reload')(__dirname);
 mb.on('ready', () => {
     console.log('app is ready');
     // your app code here
