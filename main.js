9ce30930c91c5fb29e8f351e0ec3037812bc58fa
Julien <jbertazzolambert@gmail.com>
2022-02-01 15:11:12 -0500

    works!

--- main.js
+++ main.js
@@ -13,11 +13,14 @@ require('electron-reload')(__dirname);
 mb.on('ready', () => {
     console.log('app is ready');
     // your app code here
+    mb.window.openDevTools()
+    mb.window.webContents.executeJavaScript(`
+        console.log(document.querySelector('.add-btn').addEventListener('click', (e)=>console.log('hi', e.target)));
+    `)
+    // let button = document.querySelector('.button')
+    // button.addEventListener('click', () => {addCourse()})
 });
 
 function addCourse(){
     console.log("pressed")
 }
-
-let button = document.querySelector('.button')
-button.addEventListener('click', () => {addCourse()})
