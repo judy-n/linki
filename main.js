9254867738ee4acd45fb875c79c1e059239c8dc5
judy-n <judynaamani@icloud.com>
2022-02-01 14:17:27 -0500

    pls fix

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const { menubar } = require('menubar');
 
-const mb = menubar();
+const mb = menubar({
+    preloadWindow: true,
+    browserWindow: {
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    }
+});
+let ipc = require('electron').ipcRenderer
 require('electron-reload')(__dirname);
 mb.on('ready', () => {
     console.log('app is ready');
     // your app code here
 });
+
+function addCourse(){
+    console.log("pressed")
+}
+
+let button = document.querySelector('.button')
+button.addEventListener('click', () => {addCourse()})
