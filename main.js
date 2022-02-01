133e9be4a12be972a4886c855de40cb450efe2c8
Julien <jbertazzolambert@gmail.com>
2022-02-01 15:39:54 -0500

    works!!

--- main.js
+++ main.js
@@ -15,7 +15,54 @@ mb.on('ready', () => {
     // your app code here
     mb.window.openDevTools()
     mb.window.webContents.executeJavaScript(`
-        console.log(document.querySelector('.add-btn').addEventListener('click', (e)=>console.log('hi', e.target)));
+        function addCourse() {
+            // main divs
+            const course = document.createElement('div');
+            const title = document.createElement('div');
+            const link_line = document.createElement('div');
+            const pass_line = document.createElement('div');
+            course.classList.add('course');
+            title.classList.add('title');
+            link_line.classList.add('line');
+            pass_line.classList.add('line');
+
+            // title div content
+            const className = document.createElement('h2');
+            className.appendChild(document.createTextNode("NEW"));
+            const i = document.createElement('i');
+            i.classList.add('material-icons');
+            i.appendChild(document.createTextNode('more_horiz'));
+            title.appendChild(className);
+            title.appendChild(i);
+
+            // link div content
+            const a = document.createElement('a');
+            a.setAttribute('href', 'https://utoronto.zoom.us/j/');
+            a.setAttribute('target', '_blank');
+            a.appendChild(document.createTextNode('https://utoronto.zoom.us/j/'));
+            const i_cp = document.createElement('i');
+            i_cp.classList.add('material-icons', 'copy');
+            i_cp.appendChild(document.createTextNode('content_copy'));
+            link_line.appendChild(a);
+            link_line.appendChild(i_cp);
+
+            // pass div content
+            const passText = document.createElement('span');
+            passText.classList.add('passcode');
+            passText.appendChild(document.createTextNode('Passcode: '));
+            const p = document.createElement('p');
+            p.appendChild(passText);
+            p.appendChild(document.createTextNode('123456'));
+            pass_line.appendChild(p);
+            pass_line.appendChild(i_cp);
+
+            course.appendChild(title);
+            course.appendChild(link_line);
+            course.appendChild(pass_line);
+
+            document.body.appendChild(course);
+        }
+        console.log(document.querySelector('.add-btn').addEventListener('click', addCourse));
     `)
     // let button = document.querySelector('.button')
     // button.addEventListener('click', () => {addCourse()})
