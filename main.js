6de7b749cd029d4356f15894e28031dbc150d08c
Julien <jbertazzolambert@gmail.com>
2022-02-03 22:55:49 -0500

    makes more sense

--- main.js
+++ main.js
@@ -15,49 +15,6 @@ mb.on('ready', () => {
     console.log('app is ready');
     // your app code here
     mb.window.openDevTools()
-    mb.window.webContents.executeJavaScript(`
-        function newEl(tag, classes, attrs, children) {
-            const el = document.createElement(tag);
-            el.classList.add(...classes);
-            Object.keys(attrs).forEach(attr => {
-                el.setAttribute(attr, attrs[attr]);
-            });
-            children.forEach(child => {
-                el.appendChild(child);
-            });
-            return el;
-        }
-
-        function newText(text) {
-            return document.createTextNode(text);
-        }
-
-        function addCourse() {
-            const course = newEl('div', ['course'], {}, [
-                // title
-                newEl('div', ['title'], {}, [
-                    newEl('h2', [], {}, [newText('NEW')]),
-                    newEl('i', ['material-icons'], {}, [newText('more_horiz')])
-                ]),
-                // link
-                newEl('div', ['line'], {}, [
-                    newEl('a', [], {'href': 'https://utoronto.zoom.us/j/', 'target': '_blank'}, [newText('https://utoronto.zoom.us/j/')]),
-                    newEl('i', ['material-icons', 'copy'], {}, [newText('content_copy')])
-                ]),
-                // pass
-                newEl('div', ['line'], {}, [
-                    newEl('p', [], {}, [
-                        newEl('span', ['passcode'], {}, [newText('Passcode: ')]),
-                        newText('123456')
-                    ]),
-                    newEl('i', ['material-icons', 'copy'], {}, [newText('content_copy')])
-                ])
-            ]);
-
-            document.body.appendChild(course);
-        }
-        console.log(document.querySelector('.add-btn').addEventListener('click', addCourse));
-    `)
 });
 
 function addCourse(){
